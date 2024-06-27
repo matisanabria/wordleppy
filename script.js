0e1da2dbbc22784944bef917e50baa36639e634d
matisanabria <sanabria.mati29@gmail.com>
2024-06-27 19:12:38 -0400

    Added game algorythm

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
+let intentos = 6
+const palabra = "APPLE"
+
+// Definir botón
 const button = document.getElementById("guess-button");
 button.addEventListener("click", intentar);
 
+// Leer el intento
+function leerIntento() {
+  let intento = document.getElementById("guess-input");
+  intento = intento.value;
+  intento = intento.toUpperCase();
+  return intento;
+}
+
+// Leer intento al presionar botón
 function intentar() {
+  const intento = leerIntento();
+  console.log(intento);
 
-  const INTENTO = leerIntento();
-  console.log(INTENTO);
+  
+  for (let i in palabra) {
+    if (intento[i] === palabra[i]) {
+      console.log(intento[i], "VERDE");
+    } else if (palabra.includes(INTENTO[i])) {
+      console.log(intento[i], "AMARILLO");
+    } else {
+      console.log(intento[i], "GRIS");
+    }
+  }
+  if (intento === palabra) {
+    return "ganaste";
+  }
+  intentos--
+  if (intentos===0){
+    console.log("Perdiste...")
+  }
 }
-function leerIntento(){
-    let intento = document.getElementById("guess-input");
-    intento = intento.value;
-    intento = intento.toUpperCase(); 
-    return intento;
-}
-
