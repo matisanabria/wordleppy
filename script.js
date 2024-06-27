bf4cb585e0345b4d04b000cfa943328f2a240d4c
matisanabria <sanabria.mati29@gmail.com>
2024-06-27 19:35:08 -0400

    Added letter boxes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let intentos = 6;
 const palabra = "APPLE";
 
-// Definir botón
+// Obtener elementos del HTML
 const button = document.getElementById("guess-button");
 button.addEventListener("click", intentar);
 
@@ -13,41 +13,53 @@ function leerIntento() {
   return intento;
 }
 
-
 function intentar() {
   // Leer intento al presionar botón
   const intento = leerIntento();
   console.log(intento);
+  const GRID = document.getElementById("grid");
+  const ROW = document.createElement("div");
 
-  // Verificar cada letra, si acierta "verde", si está mal la posición de la letra "amarillo", y si está mal "gris"
+  ROW.className = "row";
+
   for (let i in palabra) {
-    if (intento[i] === palabra[i]) {
-      console.log(intento[i], "VERDE");
-    } else if (palabra.includes(INTENTO[i])) {
-      console.log(intento[i], "AMARILLO");
-    } else {
-      console.log(intento[i], "GRIS");
+    const SPAN = document.createElement('span');
+    SPAN.className = 'letter';
+    // Verificar cada letra, si acierta "verde", si está mal la posición de la letra "amarillo", y si está mal "gris"
+    for (let i in palabra) {
+      if (intento[i] === palabra[i]) {
+        SPAN.innerHTML = intento[i];
+        SPAN.style.backgroundColor = "green";
+      } else if (palabra.includes(intento[i])) {
+        SPAN.innerHTML = intento[i];
+        SPAN.style.backgroundColor = "yellow";
+      } else {
+        SPAN.innerHTML = intento[i];
+        SPAN.style.backgroundColor = "grey";
+      }
+      ROW.appendChild(SPAN);
+    }
+    GRID.appendChild(ROW);
+    // Si gana se envía un mensaje de que ganó a la función terminar
+    if (intento === palabra) {
+      terminar("<h1>GANASTE!😀</h1>");
+      return;
+    }
+
+    // Se restan intentos por cada turno, si llega a 0, pierde y envía un mensaje de que perdió a la función terminar
+    console.log(intentos);
+    if (intentos === 0) {
+      terminar("<h1>PERDISTE!😖</h1>");
+    }
+
+    // Si el juego termina, deshabilita los input y muestra el mensaje enviado anteriormente
+    function terminar(mensaje) {
+      const INPUT = document.getElementById("guess-input");
+      INPUT.disabled = true;
+      button.disabled = true;
+      let contenedor = document.getElementById("guesses");
+      contenedor.innerHTML = mensaje;
     }
   }
-
-  // Si gana se envía un mensaje de que ganó a la función terminar
-  if (intento === palabra) {
-    terminar("<h1>GANASTE!😀</h1>");
-    return;
-  }
-
-  // Se restan intentos por cada turno, si llega a 0, pierde y envía un mensaje de que perdió a la función terminar
   intentos--;
-  if (intentos === 0) {
-    terminar("<h1>PERDISTE!😖</h1>");
-  }
-
-  // Si el juego termina, deshabilita los input y muestra el mensaje enviado anteriormente
-  function terminar(mensaje) {
-    const INPUT = document.getElementById("guess-input");
-    INPUT.disabled = true;
-    button.disabled = true;
-    let contenedor = document.getElementById("guesses");
-    contenedor.innerHTML = mensaje;
-  }
 }
